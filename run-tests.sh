cfe2650a1c42dba8051a570e411c3aa16f19a5c1
Timothy W. Hnat <twhnat@memphis.edu>
2017-01-03 16:24:26 -0600

    Looking for pyspark in environment

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$PYTHONPATH
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$PYTHONPATH
 
 python -m unittest discover
