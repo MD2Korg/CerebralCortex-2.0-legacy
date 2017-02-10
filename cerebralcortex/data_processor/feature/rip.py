804481a7d3e9c0479c4d9ceb07dc4b64f4776f09
Nazir Saleheen <nazir.saleheen@gmail.com>
2017-02-10 14:48:23 -0600

    RIP feature vector implemented (#36)
    * - Implemented respiration feature vector
    - Added simple test cases for feature vector (Will update later)

---
+++ cerebralcortex/data_processor/feature/rip.py
@@ -0,0 +1,177 @@
+# Copyright (c) 2017, MD2K Center of Excellence
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import numpy as np
+from cerebralcortex.kernel.datatypes.datapoint import DataPoint
+from cerebralcortex.kernel.datatypes.datastream import DataStream
+
+def rip_feature_computation(peaks_datastream: DataStream,
+                            valleys_datastream: DataStream):
+    """
+    Respiration Feature Implementation. The respiration feature values are
+    derived from the following paper:
+    'puffMarker: a multi-sensor approach for pinpointing the timing of first lapse in smoking cessation'
+    :param peaks_datastream: DataStream
+    :param valleys_datastream: DataStream
+    :return: RIP Feature DataStreams
+    """
+
+    if peaks_datastream is None or valleys_datastream is None:
+        return None
+
+    if len(peaks_datastream.datapoints) == 0 or len(valleys_datastream.datapoints) == 0:
+        return None
+
+    peaks = np.array([(i._start_time, i.sample) for i in peaks_datastream.datapoints])
+    valleys = np.array([(i._start_time, i.sample) for i in valleys_datastream.datapoints])
+
+    # initialize each rip feature array
+
+    insp = []               #1 Inhalation duration
+    expr = []               #2 Exhalation duration
+    resp = []               #3 Respiration duration
+    ieRatio = []            #4 Inhalation and Exhalation ratio
+    stretch = []            #5 Stretch
+    u_stretch = []          #6. U_Stretch = max(sample[j])
+    l_stretch = []          #7. L_Stretch = min(sample[j])
+    roc_max = []            #8. ROC_MAX = max(sample[j]-sample[j-1])
+    roc_min = []            #9. ROC_MIN = min(sample[j]-sample[j-1])
+    bd_insp = []            #10. BD_INSP = INSP(i)-INSP(i-1)
+    bd_expr = []            #11. BD_EXPR = EXPR(i)-EXPR(i-1)
+    bd_resp = []            #12. BD_RESP = RESP(i)-RESP(i-1)
+    bd_stretch = []         #14. BD_Stretch= Stretch(i)-Stretch(i-1)
+    fd_insp = []            #19. FD_INSP = INSP(i)-INSP(i+1)
+    fd_expr = []            #20. FD_EXPR = EXPR(i)-EXPR(i+1)
+    fd_resp = []            #21. FD_RESP = RESP(i)-RESP(i+1)
+    fd_stretch = []         #23. FD_Stretch= Stretch(i)-Stretch(i+1)
+    d5_expr = []            #29. D5_EXPR(i) = EXPR(i) / avg(EXPR(i-2)...EXPR(i+2))
+    d5_stretch = []         #32. D5_Stretch = Stretch(i) / avg(Stretch(i-2)...Stretch(i+2))
+
+    nCycle = len(valleys)-1 #Number of respiration cycle
+
+    for i in range(nCycle):
+        insp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=(peaks[i][0]-valleys[i][0]).total_seconds()))
+        expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=(valleys[i+1][0]-peaks[i][0]).total_seconds()))
+        resp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=(valleys[i+1][0]-valleys[i][0]).total_seconds()))
+
+        ier = (peaks[i][0]-valleys[i][0]) / (valleys[i+1][0]-peaks[i][0])
+        ieRatio.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=ier))
+        stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=peaks[i][1]-valleys[i+1][1]))
+        u_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=(peaks[i][1]-valleys[i+1][1])/2))
+        l_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=(-peaks[i][1]+valleys[i+1][1])/2))
+
+    for i in range(nCycle):
+        if i == 0:
+            bd_insp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            bd_expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            bd_resp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            bd_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+        else:
+            bd_insp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=insp[i].sample-insp[i-1].sample))
+            bd_expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=expr[i].sample-expr[i-1].sample))
+            bd_resp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=resp[i].sample-resp[i-1].sample))
+            bd_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=stretch[i].sample-stretch[i-1].sample))
+
+        if i == nCycle-1:
+            fd_insp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            fd_expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            fd_resp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+            fd_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=0))
+        else:
+            fd_insp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=insp[i].sample-insp[i+1].sample))
+            fd_expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=expr[i].sample-expr[i+1].sample))
+            fd_resp.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=resp[i].sample-resp[i+1].sample))
+            fd_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=stretch[i].sample-stretch[i+1].sample))
+
+        d5Stretch = 0
+        d5Exp = 0
+        cnt = 0
+        for j in [-2, -1, 1, 2]:
+            if i+j < 0 or i+j >= nCycle:
+                continue
+            d5Stretch = d5Stretch + stretch[i+j].sample
+            d5Exp = d5Exp + expr[i+j].sample
+            cnt = cnt+1
+
+        d5Stretch = d5Stretch/cnt
+        d5Exp = d5Exp/cnt
+        d5_stretch.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=stretch[i].sample/d5Stretch ))
+        d5_expr.append(DataPoint.from_tuple(start_time=valleys[i][0], sample=expr[i].sample/d5Exp))
+
+
+    insp_datastream = DataStream.from_datastream([peaks_datastream])
+    insp_datastream.datapoints = insp
+
+    expr_datastream = DataStream.from_datastream([peaks_datastream])
+    expr_datastream.datapoints = expr
+
+    resp_datastream = DataStream.from_datastream([peaks_datastream])
+    resp_datastream.datapoints = resp
+
+    ieRatio_datastream = DataStream.from_datastream([peaks_datastream])
+    ieRatio_datastream.datapoints = ieRatio
+
+    stretch_datastream = DataStream.from_datastream([peaks_datastream])
+    stretch_datastream.datapoints = stretch
+
+    uStretch_datastream = DataStream.from_datastream([peaks_datastream])
+    uStretch_datastream.datapoints = u_stretch
+
+    lStretch_datastream = DataStream.from_datastream([peaks_datastream])
+    lStretch_datastream.datapoints = l_stretch
+
+    bd_insp_datastream = DataStream.from_datastream([peaks_datastream])
+    bd_insp_datastream.datapoints = bd_insp
+
+    bd_expr_datastream = DataStream.from_datastream([peaks_datastream])
+    bd_expr_datastream.datapoints = bd_expr
+
+    bd_resp_datastream = DataStream.from_datastream([peaks_datastream])
+    bd_resp_datastream.datapoints = bd_resp
+
+    bd_stretch_datastream = DataStream.from_datastream([peaks_datastream])
+    bd_stretch_datastream.datapoints = bd_stretch
+
+    fd_insp_datastream = DataStream.from_datastream([peaks_datastream])
+    fd_insp_datastream.datapoints = fd_insp
+
+    fd_expr_datastream = DataStream.from_datastream([peaks_datastream])
+    fd_expr_datastream.datapoints = fd_expr
+
+    fd_resp_datastream = DataStream.from_datastream([peaks_datastream])
+    fd_resp_datastream.datapoints = fd_resp
+
+    fd_stretch_datastream = DataStream.from_datastream([peaks_datastream])
+    fd_stretch_datastream.datapoints = fd_stretch
+
+    d5_expr_datastream = DataStream.from_datastream([peaks_datastream])
+    d5_expr_datastream.datapoints = d5_expr
+
+    d5_stretch_datastream = DataStream.from_datastream([peaks_datastream])
+    d5_stretch_datastream.datapoints = d5_stretch
+
+    return insp_datastream, expr_datastream, resp_datastream, ieRatio_datastream, stretch_datastream, uStretch_datastream, lStretch_datastream, \
+            bd_insp_datastream, bd_expr_datastream, bd_resp_datastream, bd_stretch_datastream, fd_insp_datastream, fd_expr_datastream, \
+            fd_resp_datastream, fd_stretch_datastream, d5_expr_datastream, d5_stretch_datastream
+
---
+++ cerebralcortex/data_processor/test/test_ripfeatures.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2016, MD2K Center of Excellence
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import datetime
+import gzip
+import os
+import unittest
+
+import pytz
+
+from cerebralcortex.data_processor.feature.rip import rip_feature_computation
+from cerebralcortex.data_processor.signalprocessing.rip import compute_peak_valley
+from cerebralcortex.kernel.datatypes.datapoint import DataPoint
+from cerebralcortex.kernel.datatypes.datastream import DataStream
+
+class TestRIPFeatures(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestRIPFeatures, cls).setUpClass()
+        tz = pytz.timezone('US/Eastern')
+        rip = []
+        rip_sampling_frequency = 64.0/3
+        with gzip.open(os.path.join(os.path.dirname(__file__), 'res/rip.csv.gz'), 'rt') as f:
+            for l in f:
+                values = list(map(int, l.split(',')))
+                rip.append(
+                    DataPoint.from_tuple(datetime.datetime.fromtimestamp(values[0] / 1000000.0, tz=tz), values[1]))
+
+        rip_ds = DataStream(None, None)
+        rip_ds.datapoints = rip
+
+        cls.peak, cls.valley = compute_peak_valley(rip_ds,fs=rip_sampling_frequency)
+
+    def test_rip_feature_computation(self):
+        print("peak size = ",len(self.peak.datapoints))
+        print("valley size = ", len(self.valley.datapoints))
+
+        insp_datastream, expr_datastream, resp_datastream, ieRatio_datastream, stretch_datastream, uStretch_datastream, lStretch_datastream, \
+        bd_insp_datastream, bd_expr_datastream, bd_resp_datastream, bd_stretch_datastream, fd_insp_datastream, fd_expr_datastream, \
+        fd_resp_datastream, fd_stretch_datastream, d5_expr_datastream, d5_stretch_datastream = rip_feature_computation(self.peak, self.valley)
+
+        # test all are DataStream
+        self.assertIsInstance(insp_datastream, DataStream)
+        self.assertIsInstance(expr_datastream, DataStream)
+        self.assertIsInstance(resp_datastream, DataStream)
+        self.assertIsInstance(ieRatio_datastream, DataStream)
+        self.assertIsInstance(stretch_datastream, DataStream)
+        self.assertIsInstance(uStretch_datastream, DataStream)
+        self.assertIsInstance(lStretch_datastream, DataStream)
+        self.assertIsInstance(bd_insp_datastream, DataStream)
+        self.assertIsInstance(bd_expr_datastream, DataStream)
+        self.assertIsInstance(bd_resp_datastream, DataStream)
+        self.assertIsInstance(bd_stretch_datastream, DataStream)
+        self.assertIsInstance(fd_insp_datastream, DataStream)
+        self.assertIsInstance(fd_expr_datastream, DataStream)
+        self.assertIsInstance(fd_resp_datastream, DataStream)
+        self.assertIsInstance(fd_stretch_datastream, DataStream)
+        self.assertIsInstance(d5_expr_datastream, DataStream)
+        self.assertIsInstance(d5_stretch_datastream, DataStream)
+
+if __name__ == '__main__':
+    unittest.main()
